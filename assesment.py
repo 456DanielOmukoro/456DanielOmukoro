181dcc1eafd0cbfc862661b9f0b2f0b656db173c
Babatunde <92168080+456DanielOmukoro@users.noreply.github.com>
2022-02-04 15:14:52 +0000

    Add files via upload

---
+++ assesment.py
@@ -0,0 +1,50 @@
+L = [23,1,0,-12,48,15,9,20,-11,42,7,-5,13]
+overall = 0
+
+for i in range ([1,int(len(L)),2]):
+    overall=overall+i
+    print (overall)
+
+
+
+'''
+product=1
+for i in L:
+    product=product*i
+    
+print (product)
+
+
+for i in range(0,10000,1):
+    print(i)
+ 
+string=str('Code Happy')
+
+for i in str(string):
+    print(i)
+
+    
+area = 1
+num1=float(input('What is the lenght & width of a rectangle:'))
+num2=float(input('What is the lenght & width of a rectangle:'))
+
+if area>0:
+    print (num1*num2)
+   
+
+boolean = True
+while boolean:
+    user= input('Enter some words please:')
+
+    if user== 'z'or user=='Z':
+        boolean = False
+        print('You can no longer enter some words')
+    else:
+        input('Enter some more words please')
+        
+
+
+file=open('CS04022022.txt','w')
+file.write('Leunig –How To Get There/n''n/Go to the end of the path until you get to the gate.''n/Go through the gate and head straight out towards the horizon.''n/Keep going towards the horizon.''n/Sit down and have a rest every now and again,''n/But keep on going, just keep on with it.''n/Keep on going as far as you can''n/Keep on going as far as you can')
+file.close()
+'''
